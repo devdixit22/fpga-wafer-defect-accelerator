6f5c8176aaa8959751fd31deffd5dde468166f85
devdixit22 <devdixit2202@gmail.com>
2026-05-01 22:22:36 +0530

    Implement weight-stationary processing element

--- hardware/rtl/pe.v
+++ hardware/rtl/pe.v
@@ -2,10 +2,19 @@ module pe (
     input clk,
     input reset,
 
-    input signed [7:0] activation,
+    // activation input from left PE
+    input signed [7:0] activation_in,
+
+    // weight stored inside PE
     input signed [7:0] weight,
 
+    // partial sum from upper PE
     input signed [31:0] partial_sum_in,
+
+    // activation forwarded to next PE
+    output reg signed [7:0] activation_out,
+
+    // partial sum forwarded downward
     output reg signed [31:0] partial_sum_out
 );
 
@@ -13,11 +22,15 @@ reg signed [15:0] mult_result;
 
 always @(posedge clk) begin
     if (reset) begin
+        activation_out <= 0;
         partial_sum_out <= 0;
     end
     else begin
-        mult_result = activation * weight;
+        mult_result = activation_in * weight;
+
         partial_sum_out <= partial_sum_in + mult_result;
+
+        activation_out <= activation_in;
     end
 end
 
--- hardware/tb/pe_tb.v
+++ hardware/tb/pe_tb.v
@@ -5,37 +5,36 @@ module pe_tb;
 reg clk;
 reg reset;
 
-reg signed [7:0] activation;
+reg signed [7:0] activation_in;
 reg signed [7:0] weight;
 reg signed [31:0] partial_sum_in;
 
+wire signed [7:0] activation_out;
 wire signed [31:0] partial_sum_out;
 
 pe uut (
     .clk(clk),
     .reset(reset),
-    .activation(activation),
+    .activation_in(activation_in),
     .weight(weight),
     .partial_sum_in(partial_sum_in),
+    .activation_out(activation_out),
     .partial_sum_out(partial_sum_out)
 );
 
 always #5 clk = ~clk;
 
 initial begin
-
     clk = 0;
     reset = 1;
 
-    activation = 0;
-    weight = 0;
-    partial_sum_in = 0;
+    activation_in = 0;
+    weight = 3;
+    partial_sum_in = 10;
 
     #10 reset = 0;
 
-    activation = 5;
-    weight = 3;
-    partial_sum_in = 10;
+    activation_in = 5;
 
     #20;
 
