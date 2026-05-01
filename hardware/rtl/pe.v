1c527a4e7cf9d7dec086e39b41f70f5b0367e91f
devdixit22 <devdixit2202@gmail.com>
2026-05-01 22:30:18 +0530

    Final processing element for diagonal systolic array

--- hardware/rtl/pe.v
+++ hardware/rtl/pe.v
@@ -1,20 +1,16 @@
 module pe (
+
     input clk,
     input reset,
 
-    // activation input from left PE
     input signed [7:0] activation_in,
+    input signed [7:0] weight_in,
 
-    // weight stored inside PE
-    input signed [7:0] weight,
-
-    // partial sum from upper PE
     input signed [31:0] partial_sum_in,
 
-    // activation forwarded to next PE
     output reg signed [7:0] activation_out,
+    output reg signed [7:0] weight_out,
 
-    // partial sum forwarded downward
     output reg signed [31:0] partial_sum_out
 );
 
@@ -22,15 +18,22 @@ reg signed [15:0] mult_result;
 
 always @(posedge clk) begin
     if (reset) begin
+
         activation_out <= 0;
+        weight_out <= 0;
         partial_sum_out <= 0;
+
     end
     else begin
-        mult_result = activation_in * weight;
+
+        mult_result = activation_in * weight_in;
 
         partial_sum_out <= partial_sum_in + mult_result;
 
         activation_out <= activation_in;
+
+        weight_out <= weight_in;
+
     end
 end
 
