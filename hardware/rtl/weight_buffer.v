d280e9f74fa5a7a5edbd1bd45007b9f5daff64ec
devdixit22 <devdixit2202@gmail.com>
2026-05-02 11:44:11 +0530

    Add activation and weight buffer modules

---
+++ hardware/rtl/weight_buffer.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ps
+
+module weight_buffer #(
+    parameter DATA_WIDTH = 8,
+    parameter DEPTH = 1024,
+    parameter ADDR_WIDTH = 10
+)(
+    input clk,
+
+    input write_en,
+    input [ADDR_WIDTH-1:0] write_addr,
+    input signed [DATA_WIDTH-1:0] write_data,
+
+    input read_en,
+    input [ADDR_WIDTH-1:0] read_addr,
+    output reg signed [DATA_WIDTH-1:0] read_data
+);
+
+reg signed [DATA_WIDTH-1:0] memory [0:DEPTH-1];
+
+always @(posedge clk) begin
+
+    if(write_en)
+        memory[write_addr] <= write_data;
+
+    if(read_en)
+        read_data <= memory[read_addr];
+
+end
+
+endmodule
